b34f6b8658a765c8117eb1c42453dcf63dd8eb7e
40923140 <40923140@gm.nfu.edu.tw>
2022-05-20 16:23:55 +0800

    test

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n', 'tags': '', 'url': 'About.html'}, {'title': 'TEST', 'text': '', 'tags': '', 'url': 'TEST.html'}]};
